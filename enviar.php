489023515a2bcc4f0c7a0fa1cb79fdaf5fdfaa04
rodriayala <ayalarodrigo50@gmail.com>
2019-03-19 10:09:50 -0300

    agregado el archivo php

---
+++ enviar.php
@@ -0,0 +1,37 @@
+<?php
+   //Reseteamos variables a 0.
+   $nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+   if (isset($_POST['submit'])) {
+      //Obtenemos valores input formulario
+      $nombre = $_POST['nombre'];
+      $email = $_POST['email'];
+      $subject = $_POST['subject'];   
+      $mensaje = $_POST['mensaje'];
+      $para = 'santy@seco.com.ar';
+
+      //Creamos cabecera.
+      $headers = 'De' . " " . $email . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $nombre;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Email: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Empresa: " . $subject;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $mensaje;
+      $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+         echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+         echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
+  }
+?>
